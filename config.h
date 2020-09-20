0f24f7480334757717d27571ab404c6d58389cad
高宏艳 <70525487+Ghy0202@users.noreply.github.com>
2020-09-20 10:02:59 +0800

    Add files via upload

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+/*这里将存放一些桌面工具需要的常量*/
+
+
+//主窗口的一些设置
+#define WINDOW_ICON   ":/res/icon.png"          //定义窗口图标路径
+#define WINDOW_TITLE    "Fire演示助手"        //设置窗口标题
+#define WINDOW_WIDTH 320        //固定窗口宽度
+#define WINDOW_HEIGHT  380      //固定窗口高度
+#define BACKGROUND_PATH ":/back/ground.jpg"        //背景图路径
+
+#endif // CONFIG_H
