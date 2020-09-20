51bf2c3bbf3e4ef0d01b5c499346dd586a19b11b
admire-ing <70555588+admire-ing@users.noreply.github.com>
2020-09-20 20:26:14 +0800

    Update screenshot.h

--- screenshot.h
+++ screenshot.h
@@ -13,32 +13,29 @@ public:
     ~ScreenShot();
 
 private:
-    void InitUi() override;
+    void InitUi() override;                              //布局函数
     void InitProperty() override;
-
     void SetZoomVisible(bool visible);
     void SetZoomGeometry(int x, int y, int width, int height);
-
     bool ZoomIsInArea(QWidget *Zoom, QPoint pos);
-
     void Save();
 
 protected:
-    void mousePressEvent(QMouseEvent *) override;
+    void mousePressEvent(QMouseEvent *) override;        //拖拽控件函数
     void mouseMoveEvent(QMouseEvent *) override;
     void mouseReleaseEvent(QMouseEvent *) override;
     void paintEvent(QPaintEvent *) override;
     void showEvent(QShowEvent *) override;
 
 private:
-    QWidget *m_pZoom_Top;
-    QWidget *m_pZoom_Bottom;
-    QWidget *m_pZoom_Left;
-    QWidget *m_pZoom_Right;
-    QWidget *m_pZoom_Left_Top;
-    QWidget *m_pZoom_Right_Top;
-    QWidget *m_pZoom_Left_Bottom;
-    QWidget *m_pZoom_Right_Bottom;
+    QWidget *m_pZoom_Top;             // 上边拖拽
+    QWidget *m_pZoom_Bottom;          // 下边拖拽
+    QWidget *m_pZoom_Left;            // 左边拖拽
+    QWidget *m_pZoom_Right;           // 右边拖拽
+    QWidget *m_pZoom_Left_Top;        // 左上角拖拽
+    QWidget *m_pZoom_Right_Top;       // 右上角拖拽
+    QWidget *m_pZoom_Left_Bottom;     // 左下角拖拽
+    QWidget *m_pZoom_Right_Bottom;    // 右下角拖拽
 
 
 
